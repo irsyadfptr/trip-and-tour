12f03806c79b4c06ca01e2a131dc634b5758134e
Irsyad F. Putranto <irsyad.putranto@sg-dsa.com>
2025-05-08 16:01:06 +0700

    Add oauth appwrite and gcloud (URI still mismatch)

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
-import { Header } from '~/components'
+import { Header, StatsCard, TripsCard } from '~/components'
+import { allTrips, dashboardStats, user } from '~/constants'
 
 const Dashboard = () => {
-  const user = {
-    name: 'cad'
-  }
+  
   return (
     <main className='dashboard wrapper'>
       <Header title={`Welcome ${user?.name ?? 'Guest'} 👋`} description='Track activity, trends and popular destination'/>
-      Dashboard page contents
+
+      <section className="flex flex-col gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+              <StatsCard
+                  headerTitle="Total Users"
+                  total={dashboardStats.totalUsers}
+                  currentMonthCount={dashboardStats.usersJoined.currentMonth}
+                  lastMonthCount={dashboardStats.usersJoined.lastMonth}
+              />
+              <StatsCard
+                  headerTitle="Total Trips"
+                  total={dashboardStats.totalTrips}
+                  currentMonthCount={dashboardStats.tripsCreated.currentMonth}
+                  lastMonthCount={dashboardStats.tripsCreated.lastMonth}
+              />
+              <StatsCard
+                  headerTitle="Active Users"
+                  total={dashboardStats.userRole.total}
+                  currentMonthCount={dashboardStats.userRole.currentMonth}
+                  lastMonthCount={dashboardStats.userRole.lastMonth}
+              />
+          </div>
+      </section>
+      <section className="container">
+          <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+
+          <div className='trip-grid'>
+              {allTrips.map((trip) => (
+                  <TripsCard
+                      key={trip.id}
+                      id={trip.id.toString()}
+                      name={trip.name!}
+                      imageUrl={trip.imageUrls[0]}
+                      location={trip.itinerary?.[0]?.location ?? ''}
+                      tags={trip.tags}
+                      price={trip.estimatedPrice!}
+                  />
+              ))}
+          </div>
+      </section>
     </main>
   )
 }
